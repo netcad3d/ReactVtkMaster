aa183b4394fc85d4bb12265ef11fed5257432225
zeNn-G <treroin@gmail.com>
2022-08-12 11:39:12 +0300

    Fetch

--- backend/index.js
+++ backend/index.js
@@ -31,73 +31,25 @@ app.post("/upload", (req, res) => {
   });
 });
 
-const mainFolder = "uploads";
-const folderName = "uploads";
-
 app.get("/fetchFiles", (req, res) => {
-  fs.readdir(mainFolder, (err, files) => {
-    var obj = {};
-    obj["name"] = "TestProje";
-    var key = "children";
-    obj[key] = [];
+  const files = fs.readdirSync(path.join(__dirname, "uploads"));
 
-    var newObj = {};
-    newObj["name"] = "";
-    newObj["children"] = [];
+  const response = [];
 
-    var fileNameobj = [];
+  files.forEach((file) => {
+    const extension = path.extname(file);
+    const name = path.basename(file, extension);
+    const size = fs.statSync(path.join(__dirname, "uploads", file)).size;
+    const url = `http://localhost:3000/uploads/${file}`;
 
-    files.forEach((file) => {
-      fileNameobj.push(file);
+    response.push({
+      name,
+      extension,
+      size,
+      url,
     });
-
-    var lastFileType;
-    var isFirst = true;
-
-    for (var i = 0; i < fileNameobj.length; i++) {
-      var str = fileNameobj[i].split("_");
-      var fileType = str[0];
-
-      if (isFirst) {
-        if (fileType === "0") newObj["name"] = "Yüzeyler";
-        else if (fileType === "1") newObj["name"] = "Bloklar";
-        else if (fileType === "2") newObj["name"] = "Katı Modeller";
-
-        if (fileNameobj.length === 1) obj[key].push(newObj);
-      } else {
-        if (lastFileType !== fileType) {
-          obj[key].push(newObj);
-
-          newObj = {};
-          newObj["name"] = "";
-          newObj["children"] = [];
-
-          if (fileType === "0") newObj["name"] = "Yüzeyler";
-          else if (fileType === "1") newObj["name"] = "Bloklar";
-          else if (fileType === "2") newObj["name"] = "Katı Modeller";
-        }
-        if (i == fileNameobj.length - 1) obj[key].push(newObj);
-      }
-
-      var data = {
-        id: i,
-        name: fileNameobj[i],
-        url: folderName + "/" + fileNameobj[i],
-        children: [],
-      };
-      newObj[key].push(data);
-
-      isFirst = false;
-      lastFileType = fileType;
-    }
-
-    //obj[key].push(newObj);
-
-    var main = [];
-    main.push(obj);
-    var jsonObj = JSON.stringify(main);
-    res.send(jsonObj);
-  });
+  }),
+    res.send(response);
 });
 
 app.listen(3000, () => {
