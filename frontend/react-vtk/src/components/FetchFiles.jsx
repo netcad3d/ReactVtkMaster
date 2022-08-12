d5e4a8dd0448af0bcdb6c6805f7c6af9f8291079
zeNn-G <treroin@gmail.com>
2022-08-12 16:14:08 +0300

    Fix

--- frontend/react-vtk/src/components/FetchFiles.jsx
+++ frontend/react-vtk/src/components/FetchFiles.jsx
@@ -33,7 +33,7 @@ const FetchFiles = () => {
   const deleteFile = (e, name) => {
     e.preventDefault();
     axios
-      .delete(`http://localhost:3000/uploads/${name}`)
+      .delete(`http://localhost:3000/deleteFile/${name}`)
       .then((res) => {
         res.status === 200
           ? toast.success("File Deleted Succesfully", {
@@ -67,10 +67,9 @@ const FetchFiles = () => {
     }, 0);
   };
 
-  const onVisualize=()=>{
-	console.log('visualize works');
-
-  }
+  const onVisualize = () => {
+    console.log("visualize works");
+  };
 
   return (
     <>
@@ -113,7 +112,10 @@ const FetchFiles = () => {
               >
                 Dosyayı Sil
               </button>
-              <button onClick={onVisualize} className="text-xl font-semibold text-white border-2 rounded-lg p-3 w-[200px] cursor-pointer hover:bg-white hover:text-secondary hover:border-secondary transition-all duration-200 ease-in tracking-wide">
+              <button
+                onClick={onVisualize}
+                className="text-xl font-semibold text-white border-2 rounded-lg p-3 w-[200px] cursor-pointer hover:bg-white hover:text-secondary hover:border-secondary transition-all duration-200 ease-in tracking-wide"
+              >
                 Görüntüle
               </button>
             </div>
