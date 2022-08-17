cdde300fcf61b134dd96af25bad6ee4e3f1e62a7
zeNn-G <treroin@gmail.com>
2022-08-17 09:54:12 +0300

    Signup

--- frontend/react-vtk/src/components/Signup.jsx
+++ frontend/react-vtk/src/components/Signup.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Signup = () => {
   return (
-    <div className="flex h-auto m-auto mt-20 mb-20 justify-center items-center">
-      <div className="flex w-[80%] border-2 border-secondary rounded-lg flex-col ">
+    <div className="flex h-[100vh] justify-center items-center">
+      <div className="flex w-[80%] md:w-[50%] border-2 border-secondary rounded-lg flex-col justify-center items-center">
         <h1
-          className="text-white font-bold text-2xl mt-[30px] ml-[30px]"
+          className="text-white font-bold text-2xl mt-5 text-center"
           style={{ fontFamily: '"Exo-2", sans-serif' }}
         >
           Kayıt Olun
         </h1>
-        <div className="shadow-lg p-8 text-gray-600 md:w-80">
+        <div className="shadow-lg p-8 text-secondary md:w-[500px]">
           <form action="" className="flex flex-col space-y-4">
             <div>
               <label for="" className="text-sm ">
@@ -19,7 +19,7 @@ const Signup = () => {
               <input
                 type="text"
                 placeholder="Email"
-                className="ring-1 ring-gray-300 w-full rounded-md mt-2 px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"
+                className="ring-1 ring-gray-300 w-full rounded-md mt-2 px-4 py-2 outline-none focus:ring-2 focus:ring-secondary"
               />
             </div>
             <div>
@@ -29,11 +29,11 @@ const Signup = () => {
               <input
                 type="text"
                 placeholder="Şifre"
-                className="ring-1 ring-gray-300 w-full rounded-md mt-2 px-4 py-2 outline-none focus:ring-2 focus:ring-teal-300"
+                className="ring-1 ring-gray-300 w-full rounded-md mt-2 px-4 py-2 outline-none focus:ring-2 focus:ring-secondary"
               />
             </div>
             <div className="flex justify-end">
-              <button className="btn-primary text-sm w-full xxs:w-[150px] md:w-[200px]">
+              <button className="btn-secondary text-sm w-full ss:w-[200px] md:text-lg">
                 Kayıt Ol
               </button>
             </div>
