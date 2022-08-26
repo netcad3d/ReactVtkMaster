78183f0a2578302c2eae16209cf1b139fcbc51c8
Eren <erentarak1907@gmail.com>
2022-08-26 08:54:54 +0300

    setting page design

--- frontend/react-vtk/src/pages/Settings.jsx
+++ frontend/react-vtk/src/pages/Settings.jsx
@@ -1,8 +1,22 @@
 const Settings =()=>{
 	return(
-		<div>
-			<h1>Hesap Ayarları</h1>
+		<div className="flex justify-center items-center bg-primary h-[100vh]">
+		<div className="p-4 flex flex-col justify-center items-center w-[90%] md:w-[60%] h-[60%] border-2 border-secondary rounded-lg">
+		  <div className="flex-[2] w-[full] h-[300px] mt-5" />
+		  <div className="flex-[1] flex justify-center items-center flex-col mt-3">
+			<h1 className=" text-white text-2xl text-center">
+			  Hesap Ayarları
+			</h1>
+			<button
+			  className="btn-secondary w-full md:w-[300px] mt-6"
+			  onClick={() => navigate("/login")}
+			>
+			  Hesabı sil
+			</button>
+		  </div>
 		</div>
+	  </div>
+
 	)
 }
 
