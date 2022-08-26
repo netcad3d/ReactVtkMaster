f807cdc4fffd0ac81ad2d065eab6e243c66d3225
Eren <erentarak1907@gmail.com>
2022-08-26 11:52:37 +0300

    helmet added

--- backend/routes/DeleteAccount.js
+++ backend/routes/DeleteAccount.js
@@ -1,9 +1,21 @@
 const router=require('express').Router();
 const { User } = require("../models/User");
+const requireAuth = require("../middlewares/requireAuth");
 
-router.delete('/:id', async (req, res) => {
-	  const user = await User.findByIdAndDelete(req.params.id);
-  if (!user) return res.status(404).send("User not found");
-  
-  res.send(user);
-}
+router.delete('/:id',requireAuth, async (req, res) => {
+	try {
+		console.log("heyy");
+		const user = await User.findByIdAndDelete(req.params.id);
+		
+		if (!user) return res.status(404).send("User not found");
+
+	  user.removeUser(user.username);
+		res.send({message:`User ${user.username} deleted successfully`});
+		
+	} catch (error) {
+		res.status(500).send({message:"Internal Server error while deleting user account"});
+	}
+
+});
+
+module.exports=router;
