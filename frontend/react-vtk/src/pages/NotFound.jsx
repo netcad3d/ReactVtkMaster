3ad7cd44a21a7f95f1d6afa7eab35209357472b5
zeNn-G <treroin@gmail.com>
2022-08-28 23:51:07 +0300

    Lottie

--- frontend/react-vtk/src/pages/NotFound.jsx
+++ frontend/react-vtk/src/pages/NotFound.jsx
@@ -1,19 +1,18 @@
 import React, { useRef, useEffect } from "react";
 import lottie from "lottie-web";
-import coffin from "../assets/coffinDance.json";
-import "../../src/index.css";
+import notFound from "../assets/404.json";
 
 const NotFound = () => {
-  const coffinRef = useRef(null);
+  const notFoundRef = useRef(null);
 
   useEffect(() => {
     lottie.loadAnimation({
       name: "verify",
-      container: coffinRef.current,
+      container: notFoundRef.current,
       renderer: "svg",
       loop: true,
       autoplay: true,
-      animationData: coffin,
+      animationData: notFound,
       rendererSettings: {
         preserveAspectRatio: "xMidYMid slice",
       },
@@ -25,12 +24,9 @@ const NotFound = () => {
   }, []);
 
   return (
-    <div className="flex justify-center items-center bg-primary h-[100vh]">
-      <div className="flex flex-col justify-center w-[80%] md:w-[60%] h-[70%] ">
-        <div
-          className="flex-[4] items-start w-[100%] md:w-[100%] md:h-[300px] "
-          ref={coffinRef}
-        />
+    <div className="flex justify-center items-center bg-primary">
+      <div className="flex flex-col justify-center items-center w-[90%] md:w-full mt-[200px]">
+        <div className="flex-[4] " ref={notFoundRef} />
         <div className="flex flex-[1] justify-center items-center mt-[50px] md:mt-[100px]">
           <h1
             className="text-6xl md:text-7xl text-center text-secondary tracking-wider"
